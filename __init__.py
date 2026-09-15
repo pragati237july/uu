4d160a4fc0052e81ed603f6f1cc0cf3bd3bbb42e
Pragati Shinde <abcd11101217@gmail.com>
2026-09-15 13:22:38 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,7 +1,51 @@
-# Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
-from .initialise import init, deinit, reinit, colorama_text, just_fix_windows_console
-from .ansi import Fore, Back, Style, Cursor
-from .ansitowin32 import AnsiToWin32
+from typing import Any, Optional
 
-__version__ = '0.4.6'
+from .main import dotenv_values, find_dotenv, get_key, load_dotenv, set_key, unset_key
 
+
+def load_ipython_extension(ipython: Any) -> None:
+    from .ipython import load_ipython_extension
+
+    load_ipython_extension(ipython)
+
+
+def get_cli_string(
+    path: Optional[str] = None,
+    action: Optional[str] = None,
+    key: Optional[str] = None,
+    value: Optional[str] = None,
+    quote: Optional[str] = None,
+):
+    """Returns a string suitable for running as a shell script.
+
+    Useful for converting a arguments passed to a fabric task
+    to be passed to a `local` or `run` command.
+    """
+    command = ["dotenv"]
+    if quote:
+        command.append(f"-q {quote}")
+    if path:
+        command.append(f"-f {path}")
+    if action:
+        command.append(action)
+        if key:
+            command.append(key)
+            if value:
+                if " " in value:
+                    command.append(f'"{value}"')
+                else:
+                    command.append(value)
+
+    return " ".join(command).strip()
+
+
+__all__ = [
+    "get_cli_string",
+    "load_dotenv",
+    "dotenv_values",
+    "get_key",
+    "set_key",
+    "unset_key",
+    "find_dotenv",
+    "load_ipython_extension",
+]
